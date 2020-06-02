cd25346e540f4ce4462bae60071bbb92e48304e7
Aaron Decker <decker49@gmail.com>
2020-06-02 17:57:07 -0500

    Add checkouts.clj

---
+++ src/clj_stripe/checkouts.clj
@@ -0,0 +1,33 @@
+(ns clj-stripe.checkouts
+    "Functions for Stripe Checkouts API
+     https://stripe.com/docs/api/checkout/sessions/create"
+    (:use clj-stripe.common)
+    (:require [clj-stripe.util :as util]))
+
+(defn create-checkout-session
+  "Creates a payment intent.
+  Execute with common/execute."
+  [prices mode success-url cancel-url]
+  {:operation :create-checkout-session
+   :prices prices
+   :success-url success-url
+   :cancel-url  cancel-url
+   :mode mode
+   })
+
+(defn- make-request-map [{:keys [success-url cancel-url prices mode]}]
+  (apply merge
+         {"success_url" success-url
+          "cancel_url"  cancel-url
+          "payment_method_types[]" "card"
+          "mode" mode
+          }
+         (map-indexed
+          (fn [idx {:keys [price-id quantity]}]
+            {(str "line_items[" idx "][price]") price-id
+             (str "line_items[" idx "][quantity]") quantity})
+          prices)))
+
+(defmethod execute :create-checkout-session [op-data]
+  (util/post-request *stripe-token* (str api-root "/checkout/sessions")
+                     (make-request-map op-data)))
