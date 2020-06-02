bf9264e90136023d095dca9f0847787d201ceae4
Aaron Decker <decker49@gmail.com>
2020-06-02 17:56:36 -0500

    Update Clojure to 1.10.1 so that Cider works.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject deckeraa/clj-stripe "1.0.5-SNAPSHOT"
   :description "Clojure binding for Stripe"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
 		 [clj-http "3.10.1"]])
